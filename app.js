7317c9b1ac788eb6d1177fd7c6da3616a992cacb
khodorPavel <47274659+Khodor1997@users.noreply.github.com>
2021-06-16 16:04:01 +0300

    added cheat for game

--- app.js
+++ app.js
@@ -89,3 +89,14 @@ function getRandomColor() {
     const index = Math.floor(Math.random() * colors.length)
     return colors[index]
  }
+
+ function winTheGame() {
+     function kill() {
+        const circle = document.querySelector('.circle')
+
+         if (circle) {
+            circle.click()
+         }
+     }
+     setInterval(kill, 50)
+ }
